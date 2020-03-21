86f058363147c6ec3f13ec4670a7fb8e2fbe0360
Sayaka Nogawa <hello@iamsaya.com>
2020-03-21 11:21:21 +0900

    changed layout of posts and fixed header

--- resources/views/partials/content-single.blade.php
+++ resources/views/partials/content-single.blade.php
@@ -1,27 +1,13 @@
-<article class="article" @php post_class() @endphp>
-  <section class="article__gird-container">
-    <div class="article__thumbnail">
-      @php the_post_thumbnail() @endphp
-    </div>
-    <header class="article__header">
-      @include('partials/entry-meta')
-      <h1 class="article__header-title">{!! get_the_title() !!}</h1>
-    </header>
-  </section>
+<article class="posts" @php post_class() @endphp>
 
-  <section class="article__grid">
-    <div class="article__text">
-    @php ob_start();
-      the_content('Read the full post',true);
-      $postOutput = preg_replace('/<img[^>]+./','', ob_get_contents());
-      ob_end_clean();
-      echo $postOutput; @endphp
+  <div class="posts__gird-container">
+    <div class="posts__title">
+      <h2 class="posts__header-title">{!! get_the_title() !!}</h2>
+      @include('partials/entry-meta')
     </div>
-    <div class="article__img">
-      @php preg_match_all("/(<img [^>]*>)/",get_the_content(),$matches,PREG_PATTERN_ORDER);
-      for( $i=0; isset($matches[1]) && $i < count($matches[1]); $i++ ) {
-          echo $matches[1][$i];} @endphp </div>
-  </section>
+    <div class="posts__content">{!! get_the_content() !!}</div>
+  </div>
+
 </article>
 
 @include('partials.futher-reading')
--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -3,7 +3,7 @@
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <title>shossie</title>
   <meta name="description" content="">
-  <link href="https://fonts.googleapis.com/css?family=Poppins:400,500,600,700,800,900|Spectral:400,500,600,700,800&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Muli|Poppins:400,500,600,700,800,900|Spectral:400,500,600,700,800&display=swap" rel="stylesheet">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   @php wp_head() @endphp
 </head>
