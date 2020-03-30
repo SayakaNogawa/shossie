f9f842f555333161b062ac0fce0abbe1a59ebf5e
Sayaka Nogawa <hello@iamsaya.com>
2020-03-30 23:16:57 +0900

    Changed home page layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,17 +12,4 @@
 
   @include('partials.latest-archive')
 
-  @include('partials.newsletter')
-  <!-- Include popup body -->
-  @include('partials.popup-')
-  <!-- popup banner -->
-  <!-- <div class="popup__banner">
-    <a id="popping-trigger">
-      <img class="popup__banner-img" src="@asset('images/readmagazine.jpg')" alt="newsletter-banner">
-      <p>Stay Keep in Touch</p>
-    </a>
-  </div> -->
-  <!-- Category archive wrapper -->
-  @include('partials.category-archive')
-
 @endsection
